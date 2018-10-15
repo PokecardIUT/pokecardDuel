cf8d4fcf2bbbbba1f41cd11ec6aa296abe0dd34c
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-15 17:31:30 +0200

    Test monjs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const mongojs = require('mongojs')
 
 const uri = "mongodb+srv://loic:admin123@pokecardduel-f4df5.mongodb.net/Pokecard?retryWrites=true";
 
@@ -12,7 +13,7 @@ express()
   .get('/', (req, res) => {
 
     console.log("efmohezfpio");
-    var test = MongoClient.connect(uri, function(err, client) {
+  /*  var test = MongoClient.connect(uri, function(err, client) {
       const collection = client.db("Pokecard").collection("User");
       console.log(err);
       console.log(client);
@@ -20,6 +21,17 @@ express()
       // perform actions on the collection object
       client.close();
      });     
+*/
+  var db = mongojs('mongodb+srv://loic:admin123@pokecardduel-f4df5.mongodb.net/Pokecard?retryWrites=true', ['User'])
+    db.on('error', function (err) {
+        console.log('database error', err)
+    })
+    
+    db.on('connect', function () {
+        console.log('database connected')
+    })
+
+
 
     
     res.send("Project initialiser")
