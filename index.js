bb4544f5381b21c518eb63dc32b63245b7d2f6d4
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-15 17:24:37 +0200

    no message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = "mongodb+srv://loic:admin123@pokecardduel-f4df5.mongodb.net/test?retryWrites=true";
+const uri = "mongodb+srv://loic:admin123@pokecardduel-f4df5.mongodb.net/Pokecard?retryWrites=true";
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -13,8 +13,9 @@ express()
 
     console.log("efmohezfpio");
     var test = MongoClient.connect(uri, function(err, client) {
-      const collection = client.db("test").collection("devices");
-      console.log("test");      
+      const collection = client.db("Pokecard").collection("User");
+      console.log(collection);
+
       // perform actions on the collection object
       client.close();
      });     
