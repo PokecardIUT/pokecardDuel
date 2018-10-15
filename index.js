47fede15eea9dbb85322d810d85e2a2c77a36e19
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-15 15:47:28 +0200

    [TASK] Connection mongoose

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose')
 
-const uri = "mongodb+srv://loic:admin123@pokecardduel-f4df5.mongodb.net/test?retryWrites=true";
+const uri = "mongodb+srv://loic:admin123@pokecardduel-f4df5.mongodb.net/Pokecard";
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -12,16 +12,6 @@ express()
   .get('/', (req, res) => {
 
     console.log("efmohezfpio");
-    var test;
-    MongoClient.connect(uri, function(err, client) {
-      const collection = client.db("test").collection("devices");
-      test = client;
-      console.log("test");      
-      // perform actions on the collection object
-    });     
-    
-    test.close();
-
     
     res.send("Project initialiser")
   
