ba63adcaf36c7ddc89d94c627cfd831bf03a693f
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-16 09:03:22 +0200

    [FEAT] Connexion db

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
-const mongoskin = require('mongoskin')
+const mongoose = require('mongoose')
 
-const uri = "mongodb+srv://loic:admin123@pokecardduel-f4df5.mongodb.net/Pokecard?retryWrites=true";
+const uri = "mongodb://loic:<PASSWORD>@pokecardduel-shard-00-00-f4df5.mongodb.net:27017,pokecardduel-shard-00-01-f4df5.mongodb.net:27017,pokecardduel-shard-00-02-f4df5.mongodb.net:27017/test?ssl=true&replicaSet=PokecardDuel-shard-0&authSource=admin&retryWrites=true";
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -13,18 +13,28 @@ express()
   .get('/', (req, res) => {
 
 
-    MongoClient.connect(uri, function(err, client) {
-      if(err) {
-           console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+    mongoose.connect(uri).then(
+      () => {
+        console.log("Database connection established!");
+      },
+      err => {
+        console.log("Error connecting Database instance due to: ", err);
       }
-      console.log('Connected...');
-      const collection = client.db("Pokecard").collection("Users");
-      console.log(collection.find());
+    );
+    
+  //   MongoClient.connect(uri, function(err, client) {
+  //     if(err) {
+  //          console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+  //     }
+  //     console.log('Connected...');
+  //     const collection = client.db("Pokecard").collection("Users");
+  //     console.log(collection.find());
 
-      client.close();
-   });
+  //     client.close();
+  //  });
  
-
+  
+    
 
     
     res.send("Project initialiser")
