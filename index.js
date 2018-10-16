21475db3420556a106c5d152d1119421a86b753d
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-16 09:55:04 +0200

    [FEAT] Architecture route

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const mongoose = require('mongoose')
+
 const bodyParser = require('body-parser')
-const uri = "mongodb://loic:admin123@pokecardduel-shard-00-00-f4df5.mongodb.net:27017,pokecardduel-shard-00-01-f4df5.mongodb.net:27017,pokecardduel-shard-00-02-f4df5.mongodb.net:27017/Pokecard?ssl=true&replicaSet=PokecardDuel-shard-0&authSource=admin&retryWrites=true";
 
 var app = express();
 
