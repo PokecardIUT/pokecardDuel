a61df2524cd56c4fa8fa5c30dba14e8266dba74d
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-16 13:24:44 +0200

    [FEAT] add controller

---
+++ controller/auth.js
@@ -0,0 +1,21 @@
+var auth = {
+
+    login: function(req,res){
+
+        mongoose.connect(uri).then(
+            () => {
+              console.log("Database connection established!");
+            },
+            err => {
+              console.log("Error connecting Database instance due to: ", err);
+            }
+          );
+      
+          res.send("Project initialiser")
+
+
+    }
+
+
+
+}
