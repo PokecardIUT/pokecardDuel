24dbcd607ece226dada8d97a2ee325bfbb8540dd
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-16 13:47:26 +0200

    [FEAT] Change user

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uri = "mongodb://loic:admin123@pokecardduel-shard-00-00-f4df5.mongodb.net:27017,pokecardduel-shard-00-01-f4df5.mongodb.net:27017,pokecardduel-shard-00-02-f4df5.mongodb.net:27017/Pokecard?ssl=true&replicaSet=PokecardDuel-shard-0&authSource=admin&retryWrites=true";
+const uri = "mongodb://Lire:admin123@pokecardduel-shard-00-00-f4df5.mongodb.net:27017,pokecardduel-shard-00-01-f4df5.mongodb.net:27017,pokecardduel-shard-00-02-f4df5.mongodb.net:27017/Pokecard?ssl=true&replicaSet=PokecardDuel-shard-0&authSource=admin&retryWrites=true";
 
 
 var auth = {
