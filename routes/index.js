e96406659a41dfd06c52b74c3d172c00d5b32155
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-16 13:29:27 +0200

    [FIX] BUG module

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose')
-const uri = "mongodb://loic:admin123@pokecardduel-shard-00-00-f4df5.mongodb.net:27017,pokecardduel-shard-00-01-f4df5.mongodb.net:27017,pokecardduel-shard-00-02-f4df5.mongodb.net:27017/Pokecard?ssl=true&replicaSet=PokecardDuel-shard-0&authSource=admin&retryWrites=true";
 var auth = require("../controller/auth.js")
 
 router.get('/',auth.login);
