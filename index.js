d6f271d9b600f2d677d53a226d7a0007070c1ce9
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-10-16 09:26:38 +0200

    [FEAT] MERGE

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose')
-
+const bodyParser = require('body-parser')
 const uri = "mongodb://loic:admin123@pokecardduel-shard-00-00-f4df5.mongodb.net:27017,pokecardduel-shard-00-01-f4df5.mongodb.net:27017,pokecardduel-shard-00-02-f4df5.mongodb.net:27017/Pokecard?ssl=true&replicaSet=PokecardDuel-shard-0&authSource=admin&retryWrites=true";
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => {
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use( bodyParser.json() );
 
 
-    mongoose.connect(uri).then(
-      () => {
-        console.log("Database connection established!");
-      },
-      err => {
-        console.log("Error connecting Database instance due to: ", err);
-      }
-    );
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
 
-    res.send("Project initialiser")
-  
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+      mongoose.connect(uri).then(
+        () => {
+          console.log("Database connection established!");
+        },
+        err => {
+          console.log("Error connecting Database instance due to: ", err);
+        }
+      );
+
+      res.send("Project initialiser")
+    
+    })
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 
