8b8cf0cf0de17aa95d7dbaa5720076b2dd693365
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-11-05 10:08:00 +0100

    [FEAT] Prettier

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var auth = require("../controller/auth.js")
-var about = require("../controller/about.js")
+var auth = require("../controller/auth.js");
+var about = require("../controller/about.js");
 
 // ABOUT
 
-router.get('/',about.aboutApi);
+router.get("/", about.aboutApi);
 
 // AUTH
 
-router.post('/login/email', auth.loginWithEmail);
-router.post('/signup', auth.signup);
+router.post("/login/email", auth.loginWithEmail);
+router.post("/signup", auth.signup);
 
 module.exports = router;
