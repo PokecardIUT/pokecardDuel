897e40c4403c64312c3b934729ee43f3916bf573
loic <loic.ronzier@etu.univ-lyon1.fr>
2018-11-16 13:07:32 +0100

    [TASK] delete console

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+  })
+);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.all('/api/*', [require('./middlewares/validateRequest')]);
+app.all("/api/*", [require("./middlewares/validateRequest")]);
 
-app.use('/', require('./routes/routes'));
+app.use("/", require("./routes/routes"));
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
--- middlewares/validateRequest.js
+++ middlewares/validateRequest.js
@@ -1,44 +1,45 @@
-var jwt = require("jwt-simple")
+var jwt = require("jwt-simple");
 
 var verifToken = function(req, res, next) {
-
-  var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
-  var key = (req.body && req.body.x_key) || (req.query && req.query.x_key) || req.headers['x-key'];
+  var token =
+    (req.body && req.body.access_token) ||
+    (req.query && req.query.access_token) ||
+    req.headers["x-access-token"];
+  var key =
+    (req.body && req.body.x_key) ||
+    (req.query && req.query.x_key) ||
+    req.headers["x-key"];
 
   if (token || key) {
-    console.log(token)
-    console.log(key)
     try {
-      var decoded = jwt.decode(token, require('../config/secret.js')());
-      console.log(decoded);
+      var decoded = jwt.decode(token, require("../config/secret.js")());
 
       if (decoded.exp <= Date.now()) {
         res.status(400);
         res.json({
-          "status": 400,
-          "message": "Token Expired"
+          status: 400,
+          message: "Token Expired"
         });
         return;
       }
 
       next();
-  
     } catch (err) {
       res.status(500);
       res.json({
-        "status": 500,
-        "message": "Oops something went wrong",
-        "error": err
+        status: 500,
+        message: "Oops something went wrong",
+        error: err
       });
     }
   } else {
     res.status(401);
     res.json({
-      "status": 401,
-      "message": "Invalid Token or Key"
+      status: 401,
+      message: "Invalid Token or Key"
     });
     return;
   }
 };
 
-module.exports = verifToken
+module.exports = verifToken;
